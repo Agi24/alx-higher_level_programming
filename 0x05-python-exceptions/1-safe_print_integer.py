8b740b76a9960f857a6cebea6f139e8ef31fcd4c
Mary-Theresa Agi <marytheresaagi@gmail.com>
2022-06-20 04:29:58 -0700

    Writes a function that prints an integer

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-def safe_print_integer(value):
+def safe_print_interger(value):
     try:
-        print("{:d}".format(value))
-    except:
-        return (False)
-    else:
-        return (True)
+        print("{:d}" .format(value))
+        return True
+    except (ValueError, TypeError):
+        return False
