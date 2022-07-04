290ca8ef736d9bc1f69929b637fedefef23e939d
Mary-Theresa Agi <marytheresaagi@gmail.com>
2022-07-04 15:55:37 -0700

    Writes a class MyList that inherits from list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Function that creates a class MyList that inherits from list"""
+
+
 class MyList(list):
     """subclass inheriting from another class or object"""
     def print_sorted(self):
