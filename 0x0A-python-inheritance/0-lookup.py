cb20c7fa246acea912667904ea50cd7389a0cd7b
Mary-Theresa Agi <marytheresaagi@gmail.com>
2022-07-04 12:32:44 -0700

    Task 0-Writes a function that returns the list of available attributes and methods of an object

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""function that returns the list of available attributes and methods of an object:"""
+"""Defines an object attribute lookup function."""
 
 def lookup(obj):
-    """Returns a list object"""
-    return(list(dir(obj)))
+    """Return a list of an object's available attributes."""
+    return (dir(obj))
